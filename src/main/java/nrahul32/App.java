315e328c2b74960725712adecac9a8676b9ba53e
nrahul32 <rahul.eee32@gmail.com>
2020-04-18 21:03:17 +0530

    created a maven project

---
+++ src/main/java/nrahul32/App.java
@@ -0,0 +1,13 @@
+package nrahul32;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
